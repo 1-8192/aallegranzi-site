b55fc075256a0407d687cb1ec0af99fb29f0f509
1-8192 <allegranzia@gmail.com>
2019-06-24 09:36:09 -0400

    basic setup for react app

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+    <div>
+        <section className="landing-bg-image">
+            <p>test</p>
+        </section>
+    </div>
+    )
+}
+
+export default Landing
