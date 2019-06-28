4333b94bc1f372e4f5a4e28f26f5b1f8f603e967
1-8192 <allegranzia@gmail.com>
2019-06-28 19:31:21 -0400

    adding blog content

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Blog = () => {
     return (
+        <div>
         <h1>Blog</h1>
+        <iframe>
+            src="http://www.aallegranzi.com/category/coding/"
+        </iframe>
+        </div>
     )
 }
 
