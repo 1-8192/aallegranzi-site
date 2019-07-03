19eb2889bac44a0583772c60b268903e1578252b
1-8192 <allegranzia@gmail.com>
2019-07-03 15:48:22 -0400

    added brand to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,22 +6,28 @@ import Projects from './Projects'
 import Blog from './Blog'
 import Resume from './Resume'
 import About from './About'
+import Landing from './Landing'
 
 const Navbar = () => {
     return (
         <header>
+            <div> 
+                <Link className="main-nav__logo" to={"/"} component={Landing}>
+                    AA
+                </Link>
+            </div>
             <nav>
-                <ul>
-                    <li>
+                <ul class="main-nav__items">
+                    <li class="main-nav__item">
                         <Link to={"/Projects"} component={Projects}>Projects</Link>
                     </li>
-                    <li>
+                    <li class="main-nav__item">
                         <Link to={"/Blog"} component={Blog}>Blog</Link>
                     </li>
-                    <li>
+                    <li class="main-nav__item">
                         <Link to={"/Resume"} component={Resume}>Resume</Link>
                     </li>
-                    <li>
+                    <li class="main-nav__item">
                         <Link to={"/About"} component={About}>About</Link>
                     </li>
                 </ul>
