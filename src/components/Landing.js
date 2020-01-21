ca6f91ecd229f465c73386411cf8ad139c8136ab
1-8192 <allegranzia@gmail.com>
2020-01-20 22:49:42 -0500

    restyling landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,7 +10,7 @@ import About from './About'
 const Landing = () => {
     return (
     <div className="landing-bg-image">
-        <section className="landing-bg-image">
+        <section>
             <div className="landing_links">
                 <Link className="landing_link" to={"/Projects"} component={Projects}>Projects</Link>
                 <Link className="landing_link" to={"/Blog"} component={Blog}>Blog</Link>
@@ -18,6 +18,19 @@ const Landing = () => {
                 <Link className="landing_link" to={"/About"} component={About}>About</Link> 
             </div>
         </section>
+        <div classname="landing-content">
+            <p>I'm a full Stack Web Developer</p>
+            <div>
+                <ul>
+                    <li>
+                        Let's connect! <a href="https://www.linkedin.com/in/allegranzi/">LinkedIn</a>
+                    </li>
+                    <li>
+                        Check out my code on <a href="https://github.com/1-8192">Github</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
     )
 }
